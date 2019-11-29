73146b55246877d2f0b9fdcb78b65c48810e013a
Mikheil Oganesyan <mishaogan1@hotmail.com>
2019-11-29 19:24:31 +0000

    first api layer uswgi deployment read

--- flask_api/run.py
+++ flask_api/run.py
@@ -1,9 +1,9 @@
 from api.app import create_app
 from configs.flask_config import ProductionConfig
 
-application = create_app(config_object = ProductionConfig)
+app = create_app(config_object = ProductionConfig)
 
 
 if __name__ == '__main__':
-    application.run(host='0.0.0.0', use_reloader = False)
+    app.run()
     
