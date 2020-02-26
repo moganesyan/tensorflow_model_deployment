8861ce1fa79ccd4498ab473e994f9cf227bd541d
Mikheil Oganesyan <mikheil@sphere.me>
2020-02-26 17:18:55 +0000

    grpc ready deployment and client

--- tf_serving/build_image.sh
+++ tf_serving/build_image.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 docker build -t coco-mrcnn .
-docker run -p 8501:8501 -t coco-mrcnn &
+docker run -p 8500:8500 --mount type=bind,source=/Users/moganesyan/Documents/sphere_python/dev/tensorflow_model_deployment/tf_serving/serving_model,target=/models/coco_mrcnn -t coco-mrcnn &
+ 
