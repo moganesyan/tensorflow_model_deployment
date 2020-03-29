8845089bda8f68f4d81912389c969c2853b40e8e
Mikheil Oganesyan <mishaogan1@hotmail.com>
2020-03-29 02:41:53 +0100

    update notebook and gitignore

--- basic_api/app.py
+++ basic_api/app.py
@@ -7,6 +7,9 @@ import cv2
 
 import numpy as np
 
+# import tensorflow.python.keras.backend as K
+# import tensorflow as tf
+
 MRCNN_DIR = os.path.abspath('../external/mask_rcnn/')
 COCO_DIR = os.path.abspath('../external/mask_rcnn/samples')
 MODEL_DIR = os.path.abspath('../tf_serving/keras_model/')
@@ -32,34 +35,43 @@ def get_coco_config():
 
   return InferenceConfig()
 
-coco_config = get_coco_config()
-model = MaskRCNN(mode = "inference", model_dir = MODEL_DIR, config = coco_config)
-model.load_weights(MODEL_PATH, by_name = True)
-model.keras_model._make_predict_function()
-print('Loaded model...')
+# sess = K.get_session()
+# graph = sess.graph
+# graph.as_default()
+# print([n.name for n in tf.get_default_graph().as_graph_def().node])
+
+def load_model():
+  global model
+  coco_config = get_coco_config()
+  model = MaskRCNN(mode = "inference", model_dir = MODEL_DIR, config = coco_config)
+  model.load_weights(MODEL_PATH, by_name = True)
+  model.keras_model._make_predict_function()
+
+  print('Loaded model...')
+
 
 @app.route("/predict", methods=["POST"]) 
 def predict():
-    output_payload = {"success": False}
-    if flask.request.method == 'POST':
-        if flask.request.files.get("image"):
-            input_payload = flask.request.files.get("image")
-            img_stream = io.BytesIO(input_payload.read())
-            image = cv2.imdecode(np.fromstring(img_stream.read(), np.uint8), 1)
-            image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+  output_payload = {"success": False}
+  if flask.request.method == 'POST':
+      if flask.request.files.get("image"):
+          input_payload = flask.request.files.get("image")
+          img_stream = io.BytesIO(input_payload.read())
+          image = cv2.imdecode(np.fromstring(img_stream.read(), np.uint8), 1)
+          image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
-            results = model.detect([image], verbose = 1)
-            results = results[0]
-            results['rois'] = results['rois'].tolist()
-            results['class_ids'] = results['class_ids'].tolist()
-            results['scores'] = results['scores'].tolist()
-            results['masks'] = results['masks'].tolist()
-            output_payload["predictions"] = results
-            output_payload["success"] = True
+          results = model.detect([image], verbose = 1)
+          results = results[0]
+          results['rois'] = results['rois'].tolist()
+          results['class_ids'] = results['class_ids'].tolist()
+          results['scores'] = results['scores'].tolist()
+          results['masks'] = results['masks'].tolist()
+          output_payload["predictions"] = results
+          output_payload["success"] = True
 
-    return flask.jsonify(output_payload)
+  return flask.jsonify(output_payload)
 
 if __name__ == '__main__':
-    print('Running app...')
-
-    app.run(host='0.0.0.0')
+  print('Hello world')
+  load_model()
+  app.run()
