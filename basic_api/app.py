f4ee6b0e82853f0a80e4ca6079ac75db96ec0dad
Mikheil Oganesyan <mishaogan1@hotmail.com>
2020-04-06 22:44:46 +0100

    Added docstrings and changed formatting

--- basic_api/app.py
+++ basic_api/app.py
@@ -1,14 +1,9 @@
 import os
 import io
 import sys
-
-import flask
 import cv2
-
+import flask
 import numpy as np
-
-# import tensorflow.python.keras.backend as K
-# import tensorflow as tf
 
 MRCNN_DIR = os.path.abspath('../external/mask_rcnn/')
 COCO_DIR = os.path.abspath('../external/mask_rcnn/samples')
@@ -23,55 +18,57 @@ from mrcnn.model import MaskRCNN
 
 app = flask.Flask(__name__)
 
-
 def get_coco_config():
-  '''
-    Get inference config for MS-COCO
-    Ammend hyperparameters as necessary
-  '''
-  class InferenceConfig(CocoConfig):
-      GPU_COUNT = 1
-      IMAGES_PER_GPU = 1
+    '''
+    Get inference config for MS-COCO.
+    Ammend hyperparameters as necessary.
+    '''
+    class InferenceConfig(CocoConfig):
+        GPU_COUNT = 1
+        IMAGES_PER_GPU = 1
 
-  return InferenceConfig()
+    return InferenceConfig()
 
-# sess = K.get_session()
-# graph = sess.graph
-# graph.as_default()
-# print([n.name for n in tf.get_default_graph().as_graph_def().node])
 
 def load_model():
-  global model
-  coco_config = get_coco_config()
-  model = MaskRCNN(mode = "inference", model_dir = MODEL_DIR, config = coco_config)
-  model.load_weights(MODEL_PATH, by_name = True)
-  model.keras_model._make_predict_function()
-
-  print('Loaded model...')
+    """Initialises the Mask RCNN and loads weights.
+    """
+    global model
+    coco_config = get_coco_config()
+    model = MaskRCNN(mode = "inference", model_dir = MODEL_DIR, config = coco_config)
+    model.load_weights(MODEL_PATH, by_name = True)
+    model.keras_model._make_predict_function()
+    print('Loaded model...')
 
 
 @app.route("/predict", methods=["POST"]) 
 def predict():
-  output_payload = {"success": False}
-  if flask.request.method == 'POST':
-      if flask.request.files.get("image"):
-          input_payload = flask.request.files.get("image")
-          img_stream = io.BytesIO(input_payload.read())
-          image = cv2.imdecode(np.fromstring(img_stream.read(), np.uint8), 1)
-          image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    """Returns JSON object outout_payload.
+    output_payload['detections']: Output masks, rois, classes and scores for each object and image
+    output_payload['success']: True/False tag for detection success
+    """
+    output_payload = {"detections": [],"success": False}
+    if flask.request.method == 'POST':
+        if flask.request.files.get("image"):
+            input_payload = flask.request.files.get("image")
+            img_stream = io.BytesIO(input_payload.read())
 
-          results = model.detect([image], verbose = 1)
-          results = results[0]
-          results['rois'] = results['rois'].tolist()
-          results['class_ids'] = results['class_ids'].tolist()
-          results['scores'] = results['scores'].tolist()
-          results['masks'] = results['masks'].tolist()
-          output_payload["predictions"] = results
-          output_payload["success"] = True
+            image = cv2.imdecode(np.fromstring(img_stream.read(), np.uint8), 1)
+            image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
-  return flask.jsonify(output_payload)
+            results = model.detect([image], verbose = 1)
+            results = results[0]
+
+            results['rois'] = results['rois'].tolist()
+            results['class_ids'] = results['class_ids'].tolist()
+            results['scores'] = results['scores'].tolist()
+            results['masks'] = results['masks'].tolist()
+
+            output_payload["detections"] = results
+            output_payload["success"] = True
+    return flask.jsonify(output_payload)
 
 if __name__ == '__main__':
-  print('Hello world')
-  load_model()
-  app.run()
+    print('Hello world')
+    load_model()
+    app.run()
--- tf_serving/make_tf_serving.py
+++ tf_serving/make_tf_serving.py
@@ -13,134 +13,147 @@ sys.path.append(export_config.COCO_DIR)
 from coco.coco import CocoConfig
 from mrcnn.model import MaskRCNN
 
-def get_coco_config():
-  '''
-    Get inference config for MS-COCO
+def get_model_config():
+    """Returns the MS-COCO style inference config
     Ammend hyperparameters as necessary
-  '''
-  class InferenceConfig(CocoConfig):
-      GPU_COUNT = 1
-      IMAGES_PER_GPU = 1
-
-  return InferenceConfig()
+    """
+    class InferenceConfig(CocoConfig):
+        GPU_COUNT = 1
+        IMAGES_PER_GPU = 1
+    return InferenceConfig()
 
 
 def describe_graph(graph_def, show_nodes = False):
-  '''
-    Describe the graph of the tensorflow model. This is a diagnostic function.
+    """Displays the summary of the Keras/Tensorflow model graph.
+    This is a diagnostic function.
     Graph is broken down by node types: Input, output, quantization, etc.
-  '''
-  print(f"Input Feature Nodes: {[node.name for node in graph_def.node if node.op == 'Placeholder']}")
-  print(f"Unused Nodes: {[node.name for node in graph_def.node if 'unused' in node.name]}")
-  print(f"Output Nodes: {[node.name for node in graph_def.node if ('predictions' in node.name or 'softmax' in node.name)]}")
-  print(f"Quantization Node Count: {len([node.name for node in graph_def.node if 'quant' in node.name])}")
-  print(f"Constant Count: {len([node for node in graph_def.node if node.op =='Const'])}")
-  print(f"Variable Count: {len([node for node in graph_def.node if 'Variable' in node.op])}")
-  print(f"Identity Count: {len([node for node in graph_def.node if node.op =='Identity'])}")
-  print("", f"Total nodes: {len(graph_def.node)}", "")
+    graph_def: Tensorflow graph def of the model
+    """
+    print(f"Input Feature Nodes: {[node.name for node in graph_def.node if node.op == 'Placeholder']}")
+    print(f"Unused Nodes: {[node.name for node in graph_def.node if 'unused' in node.name]}")
+    print(f"Output Nodes: {[node.name for node in graph_def.node if ('predictions' in node.name or 'softmax' in node.name)]}")
+    print(f"Quantization Node Count: {len([node.name for node in graph_def.node if 'quant' in node.name])}")
+    print(f"Constant Count: {len([node for node in graph_def.node if node.op =='Const'])}")
+    print(f"Variable Count: {len([node for node in graph_def.node if 'Variable' in node.op])}")
+    print(f"Identity Count: {len([node for node in graph_def.node if node.op =='Identity'])}")
+    print("", f"Total nodes: {len(graph_def.node)}", "")
 
-  if show_nodes == True:
-    for node in graph_def.node:
-      print(f"Op:{node.op} - Name: {node.name}")
+    if show_nodes == True:
+        for node in graph_def.node:
+            print(f"Op:{node.op} - Name: {node.name}")
 
 
 def get_model_size(export_dir, version, model_file = "saved_model.pb"):
-  '''
-    Get size of the produced tf-serving model and count number of variables. This is a diagnostic function.
-  '''
+    """Displays the size and variable count of the exported model.
+    This is a diagnostic function.
+    export_dir: Path to exported model
+    version: Version number of the exported model
+    model_file: Exported model file name
+    """
 
-  model_dir = os.path.join(export_dir, str(version))
-  model_file_path = os.path.join(model_dir, model_file)
-  print(model_file_path, '')
-  pb_size = os.path.getsize(model_file_path)
-  variables_size = 0
+    model_dir = os.path.join(export_dir, str(version))
+    model_file_path = os.path.join(model_dir, model_file)
+    print(model_file_path, '')
+    pb_size = os.path.getsize(model_file_path)
+    variables_size = 0
 
-  if os.path.exists(
+    if os.path.exists(
       os.path.join(model_dir,"variables/variables.data-00000-of-00001")):
-    variables_size = os.path.getsize(os.path.join(
-        model_dir,"variables/variables.data-00000-of-00001"))
-    variables_size += os.path.getsize(os.path.join(
-        model_dir,"variables/variables.index"))
+        variables_size = os.path.getsize(os.path.join(
+            model_dir,"variables/variables.data-00000-of-00001"))
+        variables_size += os.path.getsize(os.path.join(
+            model_dir,"variables/variables.index"))
 
-  print(f"Model size: {round(pb_size / (1024.0),3)} KB")
-  print(f"Variables size: {round( variables_size / (1024.0),3)} KB")
-  print(f"Total Size: {round((pb_size + variables_size) / (1024.0),3)} KB")
+    print(f"Model size: {round(pb_size / (1024.0),3)} KB")
+    print(f"Variables size: {round( variables_size / (1024.0),3)} KB")
+    print(f"Total Size: {round((pb_size + variables_size) / (1024.0),3)} KB")
 
 
 def freeze_model(model, transforms = None, clear_devices = True):
-  input_names = [input_tensor.op.name for input_tensor in model.inputs][:4]
-  output_names = [out.op.name for out in model.outputs][:4]
-  freeze_var_names = list(set(v.op.name for v in tf.compat.v1.global_variables()))
+    """Freezes the keras/tensorflow model into a frozen graph.
+    model: Keras/Tensorflow model
+    transforms: list of optional transforms to be applied ontu the graph
+    clear_devices: Boolean flag to clear device data from graph nodes
+    Returns:
+    frozen_graph/optimized_graph: Frozen tensorflow graph
+    """
+    input_names = [input_tensor.op.name for input_tensor in model.inputs][:4]
+    output_names = [out.op.name for out in model.outputs][:4]
+    freeze_var_names = list(set(v.op.name for v in tf.compat.v1.global_variables()))
 
-  g = tf.compat.v1.get_default_graph()
-  input_graph_def = g.as_graph_def()
+    g = tf.compat.v1.get_default_graph()
+    input_graph_def = g.as_graph_def()
 
-  if clear_devices:
-      for node in input_graph_def.node:
-          node.device = ""
+    if clear_devices:
+        for node in input_graph_def.node:
+            node.device = ""
 
-  frozen_graph = tf.compat.v1.graph_util.convert_variables_to_constants(
-      master_session, input_graph_def, output_names, freeze_var_names)
+    frozen_graph = tf.compat.v1.graph_util.convert_variables_to_constants(
+        master_session, input_graph_def, output_names, freeze_var_names)
 
-  print("*" * 80)
-  print("FROZEN GRAPH SUMMARY")
-  describe_graph(frozen_graph)
-  print("*" * 80)
-
-  if transforms:
-    optimized_graph = TransformGraph(frozen_graph, input_names, output_names, transforms)
     print("*" * 80)
-    print("OPTIMIZED GRAPH SUMMARY")
-    describe_graph(optimized_graph)
+    print("FROZEN GRAPH SUMMARY")
+    describe_graph(frozen_graph)
     print("*" * 80)
-    return optimized_graph
-  else:
-    return frozen_graph
+
+    if transforms:
+        optimized_graph = TransformGraph(frozen_graph, input_names, output_names, transforms)
+        print("*" * 80)
+        print("OPTIMIZED GRAPH SUMMARY")
+        describe_graph(optimized_graph)
+        print("*" * 80)
+        return optimized_graph
+    else:
+        return frozen_graph
 
 
 def export_saved_model(export_dir, version):
-  export_dir = os.path.join(export_dir, str(version))
-  builder = saved_model.builder.SavedModelBuilder(export_dir)
-  signature = {}
+    """Exports the graph as a Tensorflow SavedModel format.
+    export_dir: Path to exported model
+    version: Version number of the exported model
+    """
+    export_dir = os.path.join(export_dir, str(version))
+    builder = saved_model.builder.SavedModelBuilder(export_dir)
+    signature = {}
 
-  g = tf.compat.v1.get_default_graph()
+    g = tf.compat.v1.get_default_graph()
 
-  input_image = saved_model.build_tensor_info(g.get_tensor_by_name("input_image:0"))
-  input_image_meta = saved_model.build_tensor_info(g.get_tensor_by_name("input_image_meta:0"))
-  input_anchors = saved_model.build_tensor_info(g.get_tensor_by_name("input_anchors:0"))
+    input_image = saved_model.build_tensor_info(g.get_tensor_by_name("input_image:0"))
+    input_image_meta = saved_model.build_tensor_info(g.get_tensor_by_name("input_image_meta:0"))
+    input_anchors = saved_model.build_tensor_info(g.get_tensor_by_name("input_anchors:0"))
 
-  output_detection = saved_model.build_tensor_info(g.get_tensor_by_name("mrcnn_detection/Reshape_1:0"))
-  output_mask = saved_model.build_tensor_info(g.get_tensor_by_name("mrcnn_mask/Reshape_1:0"))
+    output_detection = saved_model.build_tensor_info(g.get_tensor_by_name("mrcnn_detection/Reshape_1:0"))
+    output_mask = saved_model.build_tensor_info(g.get_tensor_by_name("mrcnn_mask/Reshape_1:0"))
 
-  signature[saved_model.signature_constants.DEFAULT_SERVING_SIGNATURE_DEF_KEY] = \
+    signature[saved_model.signature_constants.DEFAULT_SERVING_SIGNATURE_DEF_KEY] = \
     saved_model.signature_def_utils.build_signature_def(
-      inputs = {"input_image": input_image, "input_image_meta": input_image_meta, "input_anchors": input_anchors},
-      outputs = {"mrcnn_detection/Reshape_1": output_detection, "mrcnn_mask/Reshape_1": output_mask},
-      method_name = saved_model.signature_constants.PREDICT_METHOD_NAME)
+        inputs = {"input_image": input_image, "input_image_meta": input_image_meta, "input_anchors": input_anchors},
+        outputs = {"mrcnn_detection/Reshape_1": output_detection, "mrcnn_mask/Reshape_1": output_mask},
+        method_name = saved_model.signature_constants.PREDICT_METHOD_NAME)
 
-  builder.add_meta_graph_and_variables(export_session,
-                                       [saved_model.tag_constants.SERVING],
-                                       signature_def_map = signature)
-  builder.save()
+    builder.add_meta_graph_and_variables(export_session,
+        [saved_model.tag_constants.SERVING],
+        signature_def_map = signature)
+    builder.save()
 
 
 if __name__ == '__main__':
-  # Get coco config
-  coco_config = get_coco_config()
+    # Get model config
+    model_config = get_model_config()
 
-  # Load maask rcnn keras model and the pretrained weights
-  model = MaskRCNN(mode = "inference", model_dir = export_config.KERAS_MODEL_DIR, config = coco_config)
-  model.load_weights(export_config.KERAS_WEIGHTS_PATH, by_name = True)
+    # Load maask rcnn keras model and the pretrained weights
+    model = MaskRCNN(mode = "inference", model_dir = export_config.KERAS_MODEL_DIR, config = model_config)
+    model.load_weights(export_config.KERAS_WEIGHTS_PATH, by_name = True)
 
-  with K.get_session() as master_session:
-    graph_def = freeze_model(model.keras_model, transforms = export_config.TRANSFORMS)
+    with K.get_session() as master_session:
+        graph_def = freeze_model(model.keras_model, transforms = export_config.TRANSFORMS)
 
-    with tf.Session(graph = tf.Graph()) as export_session:
-      tf.import_graph_def(graph_def, name = "")
-      export_saved_model(export_config.EXPORT_DIR, export_config.VERSION_NUMBER)
+        with tf.Session(graph = tf.Graph()) as export_session:
+            tf.import_graph_def(graph_def, name = "")
+            export_saved_model(export_config.EXPORT_DIR, export_config.VERSION_NUMBER)
 
-  # Print the size of the tf-serving model
-  print("*" * 80)
-  get_model_size(export_config.EXPORT_DIR, export_config.VERSION_NUMBER)
-  print("*" * 80)
-  print("COMPLETED")
+    # Print the size of the tf-serving model
+    print("*" * 80)
+    get_model_size(export_config.EXPORT_DIR, export_config.VERSION_NUMBER)
+    print("*" * 80)
+    print("COMPLETED")
